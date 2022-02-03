f25526af4f08c374fe7d55544a6874262cd4f950
tunjiakanbi <tunjiakanbi@gmail.com>
2022-02-03 09:10:43 -0600

    updates

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
 import logo from './logo.svg';
 import './App.css';
+import {gsap, Power3} from 'gsap';
+import {useRef, useEffect} from 'react';
+// import { TweenLite } from 'gsap/gsap-core';
 
 function App() {
+  let logoItem = useRef(null);
+  let textItem = useRef(null);
+  useEffect(()=>{
+    gsap.fromTo(logoItem,0.8,{y:-100,opacity:0, ease:Power3.easeIn},{y:0,opacity:1});
+    gsap.to(textItem, 2,{y:-20,opacity: 1, delay:0.7, ease:Power3.easeOut})
+  },[])
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
+        <img src={logo} ref={el=>{logoItem = el}} className="App-logo" alt="logo" />
+        <p ref={el=>{textItem = el}}>
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
