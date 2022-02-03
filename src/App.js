984b02e9b5f4f188a0ffaaeda99cae9210c98c54
tunjiakanbi <tunjiakanbi@gmail.com>
2022-02-03 14:53:58 -0600

    part3 branch completed

--- src/App.js
+++ src/App.js
@@ -1,18 +1,30 @@
-import React from 'react'
-import './App.css';
-import whale from "./images/whale.jpeg"
-
-
-
+import React, {useRef, useEffect} from 'react';
+import './css/App.css';
+import whale from "./images/whale.jpeg";
+import CSSRulePlugin from 'gsap/CSSRulePlugin';
+import {gsap,  Power2} from 'gsap';
+gsap.registerPlugin(CSSRulePlugin);
 
 function App() {
+let container = useRef(null);
+let imgContainer = useRef(null);
+let image = useRef(null);
+let imageReveal = CSSRulePlugin.getRule(".img-container:after");
 
+let tl = new gsap.timeline();
+
+useEffect(()=>{
+  // console.log(imgContainer, image);
+  tl.to(container, 0, {css:{visibility:'visible'}})
+  tl.to(imageReveal, 1.4, {width: "0%", ease:Power2.easeInOut})
+  tl.from(image, 1.4, {scale: 1.6, ease: Power2.easeOut, delay: -1.4})
+});
   return (
     <section className="main">
-      <div className="container">
+      <div ref={el=>container=el} className="container">
         <>
-        <div className="img-container">
-          <img src={whale} alt="#" />
+        <div ref={el=>imgContainer=el} className="img-container">
+          <img ref={el=>image=el} src={whale} alt="#" />
         </div>
         </>
       </div>
